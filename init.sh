3eb5de4d6cfac526fe864c0fddec32f82ec23d48
darti <matthieu.dartiguenave@gmail.com>
2017-12-01 07:22:50 +0900

    Genesis mount ok

--- init.sh
+++ init.sh
@@ -2,6 +2,10 @@
 
 IMAGE=ethereum/client-go:alltools-stable
 
+docker volume create ethereum-keys
+
 docker run --rm -v $(pwd):/data $IMAGE geth --datadir /data/data/genesis init /data/genesis.json
 
-docker run --rm -v $(pwd):/data $IMAGE bootnode -genkey /data/data/keys/boot.key
+docker run --rm -v ethereum-keys:/keys $IMAGE bootnode -genkey /keys/boot.key
+
+docker run --rm -v ethereum-keys:/keys $IMAGE sh -c "bootnode -writeaddress -nodekey=/keys/boot.key > /keys/boot.url"
---
+++ member.sh
@@ -0,0 +1,6 @@
+PORT=30301
+HASH=`cat /keys/boot.url`
+BOOTNODE=getent hosts bootnode| awk '{ print $1 }'
+ENODE=enode://$HASH@$BOOTNODE:$PORT
+
+geth --datadir /data --networkid 666 --bootnodes $ENODE
