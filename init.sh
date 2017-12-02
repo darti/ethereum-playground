1e05c2c092f8cf6813183ac49606a39763f2d948
darti <matthieu.dartiguenave@gmail.com>
2017-12-02 14:49:49 +0900

    Setup

--- init.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-IMAGE=ethereum/client-go:alltools-stable
-
-docker run --rm -v $(pwd)/conf:/keys $IMAGE bootnode -genkey /keys/boot.key
-docker run --rm -v $(pwd)/conf:/keys $IMAGE sh -c "bootnode -writeaddress -nodekey=/keys/boot.key > /keys/boot.url"
