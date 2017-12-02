f83737f0bdd06d7e0192478359e50430f5f73642
darti <matthieu.dartiguenave@gmail.com>
2017-12-02 14:31:15 +0900

    Docker-compose setup override

--- init.sh
+++ init.sh
@@ -2,11 +2,5 @@
 
 IMAGE=ethereum/client-go:alltools-stable
 
-docker volume create ethereum-keys
-docker volume create ethereum-master
-
-docker run --rm -v ethereum-master:/data -v $(pwd):/conf/ $IMAGE geth --datadir /data init /conf/genesis.json
-
-docker run --rm -v ethereum-keys:/keys $IMAGE bootnode -genkey /keys/boot.key
-
-docker run --rm -v ethereum-keys:/keys $IMAGE sh -c "bootnode -writeaddress -nodekey=/keys/boot.key > /keys/boot.url"
+docker run --rm -v $(pwd)/conf:/keys $IMAGE bootnode -genkey /keys/boot.key
+docker run --rm -v $(pwd)/conf:/keys $IMAGE sh -c "bootnode -writeaddress -nodekey=/keys/boot.key > /keys/boot.url"
